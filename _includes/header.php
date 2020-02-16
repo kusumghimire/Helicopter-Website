b9b435b0020879b40b3d5828c8d357c3d64d6426
kusum_ghimire <kusumghimire@abhiyantrik.com>
2020-02-16 08:02:13 +0545

    adding repository

---
+++ _includes/header.php
@@ -0,0 +1,24 @@
+<section>
+  <nav class="navbar  navbar-expand-sm">
+    <a class="navbar-brand" href="#">
+    <img class="navbar-img" src=".\img\helicopter.png" atl="anil chitrakar pic" title="anil chitrakar pic">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-list-2"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbar-list-2">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Blog</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</section>
