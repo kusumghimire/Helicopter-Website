4b6ab8dd0b782ceb5dac27b4ed043bd82e52a121
kusum_ghimire <kusumghimire@abhiyantrik.com>
2020-02-17 00:10:56 +0545

    banner-contents added

--- _includes/banner.php
+++ _includes/banner.php
@@ -2,4 +2,14 @@
   <div class="banner-image">
     <img src="./img/banner-img.png">
   </div>
+  <div class="shadow-img">
+    <img src="./img/img-shadow.png">
+  </div>
+  <div class="banner-contents">
+    <h1> exceeding expectations around the world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    </p>
+    <span><i class="fas fa-arrow-right"></i>Read more</span>
+
+  </div>
 </section>
