ed25a2a04182b44c39be7cf4fa1731e4d90ffd24
kusum_ghimire <kusumghimire@abhiyantrik.com>
2020-02-18 23:44:23 +0545

    service.php with background-image and h1 is created

---
+++ _includes/service.php
@@ -0,0 +1,13 @@
+<section class="service-section">
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <h1>
+                <strong>SERVICES</strong>
+                OFFERED
+            </h1>
+        </div>
+    </div>
+</div>
+</section>
+
