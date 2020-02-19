1cb2ce10126eb9d6a8c69ff33d19648408a06ca4
kusum_ghimire <kusumghimire@abhiyantrik.com>
2020-02-19 11:13:49 +0545

    image animation added to service section

--- _includes/service.php
+++ _includes/service.php
@@ -1,13 +1,44 @@
 <section class="service-section">
-<div class="container">
+  <div class="container">
     <div class="row">
-        <div class="col-md-6">
-            <h1>
-                <strong>SERVICES</strong>
-                OFFERED
-            </h1>
-        </div>
-    </div>
-</div>
-</section>
+      <div class="col-md-6 service-title">
+        <h1>
+          <strong>SERVICES</strong>
+          OFFERED
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt animi quam provident doloremque
+          minus amet
+          Lorem ipsum dolor sit a
 
+        </p>
+      </div>
+    </div>
+  </div>
+  <section class="service-gallery">
+    <div class="container">
+      <div class="row">
+        <div class="service-img">
+          <img src="./img/img-1.png">
+        </div>
+        <div class="service-img">
+          <img src="./img/img-2.png">
+        </div>
+        <div class=service-img>
+          <img src="./img/holy-visit.png">
+        </div>
+        <div class="service-img">
+          <img src="./img/skating.png">
+        </div>
+        <div class="service-img">
+          <img src="./img/jungle.png">
+        </div>
+        <!-- <div class="service-img">
+          <figure>
+           <img src="./img/img-shadow.png">
+          </figure> 
+        </div> -->
+      </div>
+    </div>
+  </section> <!-- section gallery ends here -->
+</section><!-- section-section ends here -->
