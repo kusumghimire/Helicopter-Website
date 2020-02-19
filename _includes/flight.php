e0aaa869fc476e617de9ea49ca865da34eb16555
kusum_ghimire <kusumghimire@abhiyantrik.com>
2020-02-19 11:53:35 +0545

    fleet-banner is created

--- _includes/flight.php
+++ _includes/flight.php
@@ -6,6 +6,22 @@
           <h1>OUR <strong>FLEET</strong></h1>
         </div>
       </div>
+      <div class="col-md-8">
+        <div class="banner">
+          <img src="./img/fleet-img.png">
+        </div>
+        <div class="fleet-contents">
+          <span>Lorem ipsum dolor sit amet.</span>
+        <h1><strong>Airbus</strong> AS 350B3E</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus dolorum voluptate blanditiis, ullam incidunt fugiat ipsa at quibusdam unde quis?
+        </p>
+        <hr>
+      </div>
+      </div>
+      <div class="col-md-1">
+        icon
+      </div>
     </div>
   </div>
 </section>
