b0c86bb41c956a0d53df73ccefc9d0136cc22a16
kusum_ghimire <kusumghimire@abhiyantrik.com>
2020-02-19 11:35:21 +0545

    flight.php is created

---
+++ _includes/flight.php
@@ -0,0 +1,11 @@
+<section class="fleet-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <div class="fleet-title">
+          <h1>OUR <strong>FLEET</strong></h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
