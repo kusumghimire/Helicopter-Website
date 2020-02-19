caf9201ebc8e4022cb13c00fddf30fdbb09c62c0
kusum_ghimire <kusumghimire@abhiyantrik.com>
2020-02-19 21:55:12 +0545

    partner.php is created

--- _includes/expert.php
+++ _includes/expert.php
@@ -15,16 +15,69 @@
           <div class="tickets">
             <div class="title">Mountain FLights and Heli Treks</div>
             <div class="wrap">
-              <div class="div1"></div>
-              <div class="div1"></div>
-              <div class="div1"></div>
+              <div class="div1">+977 ********</div>
+              <div class="div1">+977 ********</div>
+              <div class="div1">+977 ********</div>
+            </div>
+          </div>
+          <div class="tickets">
+            <div class="title">Rescue Flight</div>
+            <div class="wrap">
+              <div class="div1">+977 ********</div>
+              <div class="div1">+977 ********</div>
+              <div class="div1">+977 ********</div>
+            </div>
+          </div>
+          <div class="tickets">
+            <div class="title">Email</div>
+            <div class="wrap">
+              <div class="div1" id="email">contact@carenepal.com</div>
+              <div class="div1" id="email">emergency@carenepal.com</div>
             </div>
           </div>
         </div>
       </div>
       <div class="col-md-2"></div>
       <div class="col-md-5">
-        <div class="expert-image">
+        <div class="expert-video">
+          <div class="expert-image">
+            <img src=".\img\expert-img.png">
+          </div>
+          <div class="expert-absolute">
+            <div class="expert-icon">
+              <div class="about-icons">
+                <figure>
+                  <img src=".\img\helicopter-icon.svg">
+                </figure>
+                <span class="pic-title">Hours Logged</span>
+                <span class="pic-number">520</span>
+              </div>
+              <div class="circle">
+              </div>
+            </div>
+            <div class="expert-icon">
+              <div class="about-icons">
+                <figure>
+                  <img src=".\img\team.svg">
+                </figure>
+                <span class="pic-title">Passenger Server</span>
+                <span class="pic-number">80000+</span>
+              </div>
+              <div class="circle">
+              </div>
+            </div>
+            <div class="expert-icon">
+              <div class="about-icons">
+                <figure>
+                  <img src=".\img\helicopter-icon.svg">
+                </figure>
+                <span class="pic-title">Cargo Transported</span>
+                <span class="pic-number">5.2tones</span>
+              </div>
+              <div class="circle">
+              </div>
+            </div>
+          </div>
 
         </div>
       </div>
---
+++ _includes/partners.php
@@ -0,0 +1,68 @@
+<section class="partner-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 partner-title">
+        <h1>
+          OUR <strong>PARTNERS</strong>
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt animi quam provident doloremque
+          minus amet
+          Lorem ipsum dolor sit a
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="partner-gallery">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <div class="partner-img">
+            <img src="./img/partner-1.png">
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="partner-img">
+            <img src="./img/partner-2.png">
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="partner-img">
+            <img src="./img/partner-3.png">
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="partner-img">
+            <img src="./img/partner-4.png">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="partner-gallery1">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <div class="partner-img">
+            <!-- <img src="./img/partner-1.png"> -->
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="partner-img">
+            <img src="./img/partner-5.png">
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="partner-img">
+            <img src="./img/partner-6.png">
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="partner-img">
+            <!-- <img src="./img/partner-4.png"> -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <section>
