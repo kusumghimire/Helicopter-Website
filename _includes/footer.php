81771e1f23d589fc69a599c5d91bd9d45d46bab0
kusum_ghimire <kusumghimire@abhiyantrik.com>
2020-02-21 10:17:18 +0545

    helicopter-icon with color white  added to the header

---
+++ _includes/footer.php
@@ -0,0 +1,22 @@
+<footer>
+  <div class="container">
+    <div class="section-footer">
+      <div class="footer-links">
+        <nav>
+          <ul>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Faq</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">enquire</a></li>
+          </ul>
+        </nav>
+        <div class="copyright">
+          <i class="fas fa-copyright"></i> Copyright,company name.All right reserved
+        </div>
+      </div>
+      <div class="footer-icons">
+
+      </div>
+    </div>
+  </div>
+</footer>
