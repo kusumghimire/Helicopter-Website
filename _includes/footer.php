3f5861730cae871e0111123932301d8a76f437a9
kusum_ghimire <kusumghimire@abhiyantrik.com>
2020-03-12 09:09:37 +0545

    css property added to footer icons

--- _includes/footer.php
+++ _includes/footer.php
@@ -18,7 +18,7 @@
         <ul>
           <li><a href="#"><i class="fab fa-facebook"></i></a></li>
           <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-          <li><a href="#"></a><i class="fab fa-instagram"></i></li>
+          <li><a href="#"><i class="fab fa-instagram"></i></a></li>
         </ul>
       </div>
     </div>
